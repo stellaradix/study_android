4782476047ef1472eb40ced52953062aca1591e4
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-02-13 17:23:39 +0900

    anchor1

--- compose_basic/app02_happy_birthday/src/main/java/codelab/compose/happybirthday/MainActivity.kt
+++ compose_basic/app02_happy_birthday/src/main/java/codelab/compose/happybirthday/MainActivity.kt
@@ -3,11 +3,15 @@ package codelab.compose.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import codelab.compose.happybirthday.ui.theme.HappyBirthdayTheme
@@ -21,7 +25,7 @@ class MainActivity : ComponentActivity() {
 				Surface(
 					color = MaterialTheme.colorScheme.background
 				) {
-					BirthdayGreetingWithText("Happy Birthday Sam!", "- from Emma")
+					BirthdayGreetingWithImage("Happy Birthday Sam!", "- from Emma")
 				}
 			}
 		}
@@ -43,10 +47,22 @@ fun BirthdayGreetingWithText(message: String, from: String) {
 	}
 }
 
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+	val image = painterResource(R.drawable.androidparty)
+	Box {
+		Image(
+			painter = image,
+			contentDescription = null
+		)
+		BirthdayGreetingWithText(message = message, from = from)
+	}
+}
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
 	HappyBirthdayTheme {
-		BirthdayGreetingWithText("Happy Birthday Sam!", "- from Emma")
+		BirthdayGreetingWithImage("Happy Birthday Sam!", "- from Emma")
 	}
 }
