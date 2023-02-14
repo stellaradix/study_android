d817f5eb55e6a21c1f8f037ebddc4e5d157f7dc5
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-02-14 15:12:58 +0900

    Compose Basic App04: Practice - FontWeight
    FontWeight
    - 1...1000
    - W100...W900
    - Thin, ExtraLight, Light, Normal, Medium, SemiBold, Bold, ExtraBold, Black
    
    === === ===
    Course: Android Basics with Compose
    Unit 1: Your first Android app
    Path 3: Build a basic layout

--- compose_basic/app04_task_manager/src/main/java/com/example/taskmanager/MainActivity.kt
+++ compose_basic/app04_task_manager/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +33,61 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					TaskManagerApp()
 				}
 			}
 		}
 	}
 }
 
-@Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	TaskManagerTheme {
-		Greeting("Android")
+	Surface(
+		modifier = Modifier.fillMaxSize(),
+		color = MaterialTheme.colorScheme.background
+	) {
+		TaskManagerApp()
+	}
+}
+
+@Composable
+fun TaskManagerApp() {
+	Surface(
+		modifier = Modifier.fillMaxSize(),
+		color = MaterialTheme.colorScheme.background
+	) {
+		TaskManager(
+			stringResource(R.string.all_tasks_completed),
+			stringResource(R.string.nice_work),
+			painterResource(R.drawable.ic_task_completed)
+		)
+	}
+}
+
+@Composable
+fun TaskManager(
+	notice: String,
+	message: String,
+	imagePainter: Painter
+) {
+	Column(
+		modifier = Modifier.fillMaxSize(),
+		verticalArrangement = Arrangement.Center,
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+		Image(
+			painter = imagePainter,
+			contentDescription = null
+		)
+		Text(
+			text = notice,
+			fontWeight = FontWeight.Bold,
+			modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+		)
+		Text(
+			text = message,
+			fontSize = 16.sp
+		)
 	}
 }
