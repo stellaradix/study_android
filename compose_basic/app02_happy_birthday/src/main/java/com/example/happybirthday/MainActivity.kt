3b5595fdba0f9a02a813b8d1acecf57195baf285
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-02-14 10:39:48 +0900

    Compose Basic App02: Text composable and UI Hierarchy
    UI: User Interface
    - How UI component layout on screen
    - How UI component interact with User
    
    Annotation
    - attached extra infomation to code
    - help tools and developer
    - can take parameters
    
    Text component
    - parameter > text: String
    - parameter > fontSize: TextUnit (~.dp, ~.sp, ..)
    
    UI Hierarchy
    - conponent can contain other components
    - Column: align vertically
    - Row: align horizontally
    
    === === ===
    Course: Android Basics with Compose
    Unit 1: Your first Android app
    Path 3: Build a basic layout

--- compose_basic/app02_happy_birthday/src/main/java/com/example/happybirthday/MainActivity.kt
+++ compose_basic/app02_happy_birthday/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +19,9 @@ class MainActivity : ComponentActivity() {
 			HappyBirthdayTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
-					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					BirthdayGreetingWithText("Happy Birthday Sam!", "- from Emma")
 				}
 			}
 		}
@@ -30,14 +29,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun BirthdayGreetingWithText(message: String, from: String) {
+	Column {
+		Text(
+			text = message,
+			fontSize = 30.sp,
+		)
+
+		Text(
+			text = from,
+			fontSize = 24.sp
+		)
+	}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
 	HappyBirthdayTheme {
-		Greeting("Android")
+		BirthdayGreetingWithText("Happy Birthday Sam!", "- from Emma")
 	}
 }
