c2e1733367dd22e8d06748ec8823db6c6491c50e
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-02-14 11:56:17 +0900

    Compose Basic App02: Image composable, Align, Arrange, Modifier
    UI > verticalArrangement: Arrangement
    - Equal Weight, Space Between, Space Around, Space Evenly
    - Top, Center, Bottom
    
    UI > horizontalAlignment: Alignment
    - Equal Weight, Space Between, Space Around, Space Evenly
    - Start (LTR), Center, End (LTR)
    
    Modifier
    - .padding(top, bottom, start, end)
    - .align(alignment)
    - .fillMaxSize()
    
    androidx.compose.ui.res
    - painterResource(R.drawable.~): Painter
    - stringResource(R.string.~): String
    
    Image component
    - parameter > painter: Painter
    - parameter > contentDescription: String
    - parameter > contentScale: ContentScale
    
    === === ===
    Course: Android Basics with Compose
    Unit 1: Your first Android app
    Path 3: Build a basic layout

--- compose_basic/app02_happy_birthday/src/main/java/com/example/happybirthday/MainActivity.kt
+++ compose_basic/app02_happy_birthday/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -21,7 +28,10 @@ class MainActivity : ComponentActivity() {
 				Surface(
 					color = MaterialTheme.colorScheme.background
 				) {
-					BirthdayGreetingWithText("Happy Birthday Sam!", "- from Emma")
+					BirthdayGreetingWithImage(
+						stringResource(R.string.happy_birthday_text),
+						stringResource(R.string.signature_text)
+					)
 				}
 			}
 		}
@@ -30,23 +40,51 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BirthdayGreetingWithText(message: String, from: String) {
-	Column {
+	Column(
+		modifier = Modifier.fillMaxSize(),
+		verticalArrangement = Arrangement.Top,
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
 		Text(
 			text = message,
 			fontSize = 30.sp,
+			modifier = Modifier.padding(top = 16.dp)
 		)
 
 		Text(
 			text = from,
-			fontSize = 24.sp
+			fontSize = 24.sp,
+			modifier = Modifier
+				.padding(top = 16.dp, end = 16.dp)
+				.align(alignment = Alignment.End)
 		)
 	}
 }
 
-@Preview(showBackground = true)
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+	val image = painterResource(R.drawable.androidparty)
+	Box {
+		Image(
+			painter = image,
+			contentDescription = null,
+			contentScale = ContentScale.Crop
+		)
+
+		BirthdayGreetingWithText(
+			message = message,
+			from = from
+		)
+	}
+}
+
+@Preview(showBackground = false)
 @Composable
 fun DefaultPreview() {
 	HappyBirthdayTheme {
-		BirthdayGreetingWithText("Happy Birthday Sam!", "- from Emma")
+		BirthdayGreetingWithImage(
+			stringResource(R.string.happy_birthday_text),
+			stringResource(R.string.signature_text)
+		)
 	}
 }
