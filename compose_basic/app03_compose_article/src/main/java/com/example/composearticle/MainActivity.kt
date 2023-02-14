b1690fd2eb6406cef996417fc59cae0b82a2d3ea
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-02-14 14:23:51 +0900

    Compose Basic App03: Practice - Column component, TextAlign
    Column
    - align children component vertically
    
    TextAlign
    - Left, Right, Center, Justify, Start, End
    - alignment of text
    
    === === ===
    Course: Android Basics with Compose
    Unit 1: Your first Android app
    Path 3: Build a basic layout

--- compose_basic/app03_compose_article/src/main/java/com/example/composearticle/MainActivity.kt
+++ compose_basic/app03_compose_article/src/main/java/com/example/composearticle/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +31,63 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					ComposeArticleApp()
 				}
 			}
 		}
 	}
 }
 
-@Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
 	ComposeArticleTheme {
-		Greeting("Android")
+		Surface(
+			modifier = Modifier.fillMaxSize(),
+			color = MaterialTheme.colorScheme.background
+		) {
+			ComposeArticleApp()
+		}
+	}
+}
+
+@Composable
+fun ComposeArticleApp() {
+	Article(
+		title = stringResource(R.string.article_title_text),
+		shortDescription = stringResource(R.string.article_short_description),
+		longDescription = stringResource(R.string.article_long_description),
+		imagePainter = painterResource(R.drawable.bg_compose_background)
+	)
+}
+
+@Composable
+fun Article(
+	title: String,
+	shortDescription: String,
+	longDescription: String,
+	imagePainter: Painter,
+	modifier: Modifier = Modifier
+) {
+	Column(modifier = modifier) {
+		Image(
+			painter = imagePainter,
+			contentDescription = null,
+		)
+		Text(
+			text = title,
+			fontSize = 24.sp,
+			modifier = Modifier.padding(16.dp)
+		)
+		Text(
+			text = shortDescription,
+			textAlign = TextAlign.Justify,
+			modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+		)
+		Text(
+			text = longDescription,
+			textAlign = TextAlign.Justify,
+			modifier = Modifier.padding(16.dp)
+		)
 	}
 }
