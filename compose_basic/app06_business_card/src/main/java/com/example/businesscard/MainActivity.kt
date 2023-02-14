a6aed5f0ae9d48c157b39bba1c8e81e962b46ca1
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-02-15 02:26:44 +0900

    temp1

--- compose_basic/app06_business_card/src/main/java/com/example/businesscard/MainActivity.kt
+++ compose_basic/app06_business_card/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +25,40 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					BusinessCardApp()
 				}
 			}
 		}
 	}
 }
 
-@Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
 	BusinessCardTheme {
-		Greeting("Android")
+		Surface(
+			modifier = Modifier.fillMaxSize(),
+			color = MaterialTheme.colorScheme.background
+		) {
+			BusinessCardApp()
+		}
+	}
+}
+
+@Composable
+fun BusinessCardApp() {
+	Column(
+		modifier = Modifier
+			.fillMaxSize()
+			.padding(top = 16.dp, bottom = 16.dp)
+	) {
+		Column(
+			modifier = Modifier.weight(1f)
+		) {
+
+		}
+		Column {
+			Text("+082 010 1234 5678")
+		}
 	}
 }
