0423bc75d063d38e6f1b340cec1c5a8db3a8b372
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-02-15 18:38:30 +0900

    temp2

--- compose_basic/app06_business_card/src/main/java/com/example/businesscard/MainActivity.kt
+++ compose_basic/app06_business_card/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,16 +3,27 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.LocalPhone
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,15 +61,102 @@ fun BusinessCardApp() {
 	Column(
 		modifier = Modifier
 			.fillMaxSize()
-			.padding(top = 16.dp, bottom = 16.dp)
+			.background(Color(0xFF073042)),
 	) {
-		Column(
-			modifier = Modifier.weight(1f)
-		) {
+		MainBox(
+			paintImage = painterResource(R.drawable.android_logo),
+			name = stringResource(R.string.business_card_name),
+			specification = stringResource(R.string.business_card_spec),
+			modifier = Modifier
+				.fillMaxWidth()
+				.weight(1f),
+		)
+		Divider()
+		InfoBox(
+			icon = Icons.Rounded.LocalPhone,
+			info = stringResource(R.string.business_card_phone),
+		)
+		Divider()
+		InfoBox(
+			icon = Icons.Rounded.Share,
+			info = stringResource(R.string.business_card_handle),
+		)
+		Divider()
+		InfoBox(
+			icon = Icons.Rounded.Email,
+			info = stringResource(R.string.business_card_email),
+		)
+		Spacer(
+			modifier = Modifier.height(32.dp),
+		)
+	}
+}
 
-		}
-		Column {
-			Text("+082 010 1234 5678")
-		}
+@Composable
+fun MainBox(
+	paintImage: Painter,
+	name: String,
+	nameColor: Color = Color.White,
+	specification: String,
+	specificationColor: Color = Color(0xFF3DDC84),
+	modifier: Modifier = Modifier
+) {
+	Column(
+		verticalArrangement = Arrangement.Center,
+		horizontalAlignment = Alignment.CenterHorizontally,
+		modifier = modifier
+	) {
+		Image(
+			painter = paintImage,
+			contentDescription = null,
+			modifier = Modifier
+				.fillMaxWidth(0.4f)
+		)
+		Text(
+			text = name,
+			color = nameColor,
+			textAlign = TextAlign.Center,
+			fontSize = 28.sp,
+			fontWeight = FontWeight.Bold,
+			modifier = Modifier
+				.padding(top = 16.dp, bottom = 16.dp)
+				.fillMaxWidth()
+		)
+		Text(
+			text = specification,
+			color = specificationColor,
+			textAlign = TextAlign.Center,
+			fontWeight = FontWeight.Bold,
+			modifier = Modifier
+				.fillMaxWidth(),
+		)
+	}
+}
+
+@Composable
+fun InfoBox(
+	icon: ImageVector,
+	iconColor: Color = Color(0xFF3DDC84),
+	info: String,
+	infoColor: Color = Color.White,
+	modifier: Modifier = Modifier
+		.padding(start = 32.dp, end = 32.dp, top = 8.dp, bottom = 8.dp),
+) {
+	Row(
+		modifier = modifier
+			.fillMaxWidth(),
+	) {
+		Icon(
+			imageVector = icon,
+			tint = iconColor,
+			contentDescription = null,
+			modifier = Modifier
+				.padding(end = 24.dp),
+		)
+		Text(
+			text = info,
+			color = infoColor,
+			modifier = Modifier.padding(0.dp)
+		)
 	}
 }
